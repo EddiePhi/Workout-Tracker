12b019620be256f902feea4bee5532e0af00d0e3
Eddie Phimansone <ephimansone@gmail.com>
2021-02-02 10:21:20 -0500

    htmlRoutes working

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,23 @@
+// const router = require('express').Router()
+// const { join } = require('path')
+// const { Workout } = require('../models')
+const path = require("path");
+
+module.exports = function (app) {
+    // HTML routes
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    })
+
+    app.get('/exercise', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'))
+    });
+  
+    app.get('/stats', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/stats.html'))
+    });
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    })
+};
